b055e626ffb712a4301bad37ee713e6a372a79ac
vebradev <justinas@vebra.dev>
2019-07-23 14:57:40 +0300

    Add express-session and configure it

--- index.js
+++ index.js
@@ -1,7 +1,22 @@
 const express = require("express");
+const session = require("express-session");
 const server = express();
 const AccountsRouter = require('./accounts/accounts-router');
 
+server.use(
+  session({
+    name: 'rozhes',
+    secret: 'nobody tosses a dwarf!',
+    cookie: {
+      maxAge: 1 * 24 * 60 * 60 * 1000,
+      secure: false, // only set cookies over https. Server will not send back a cookie over http.
+      httpOnly: true,
+    }, // 1 day in milliseconds
+    httpOnly: true, // don't let JS code access cookies. Browser extensions run JS code on your browser!
+    resave: false,
+    saveUninitialized: false,
+  })
+);
 server.use(express.json());
 server.use('/api/', AccountsRouter);
 
